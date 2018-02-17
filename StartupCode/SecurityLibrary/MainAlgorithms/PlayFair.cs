2620554b188ef0a30d2df7c2d96176d3a0b4f995
Ibrahim Hasan <Ibrahim__Hasan@outlook.com>
2018-02-17 08:14:08 +0200

    Startup files

---
+++ StartupCode/SecurityLibrary/MainAlgorithms/PlayFair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class PlayFair : ICryptographicTechnique<string, string>
+    {
+        
+        public string Decrypt(string cipherText, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encrypt(string plainText, string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
